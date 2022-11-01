a3a2a799c9b97ab5b81c1a7f5162a439def6a7b4
Jean-Michel Depaepe <jean-michel.depaepe@equans.com>
2022-11-01 10:11:27 +0100

    demo

---
+++ src/main/java/com/example/lesson08/CircleClick.java
@@ -0,0 +1,29 @@
+package com.example.lesson08;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+public class CircleClick extends Application {
+    @Override
+    public void start(Stage stage) {
+        Canvas canvas = new Canvas(400, 300);
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+
+        canvas.setOnMouseClicked(event -> drawCircle(gc, event));
+
+        stage.setScene(new Scene(new VBox(canvas)));
+        stage.setTitle("click cercles");
+        stage.show();
+    }
+
+    private void drawCircle(GraphicsContext gc, MouseEvent event) {
+        gc.setFill(Color.AQUA);
+        gc.fillOval(event.getX() + 15, event.getY() + 15, 30, 30);
+    }
+}
