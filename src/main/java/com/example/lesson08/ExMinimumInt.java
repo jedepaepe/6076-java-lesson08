3ab1a0c898e21ec9965d87e75e60dce4d0ef4ec5
Jean-Michel Depaepe <jean-michel.depaepe@equans.com>
2022-11-07 21:22:12 +0100

    exercice

---
+++ src/main/java/com/example/lesson08/ExMinimumInt.java
@@ -0,0 +1,25 @@
+package com.example.lesson08;
+
+public class ExMinimumInt {
+    public static void main(String[] args) {
+        double a, b;
+        if (args.length == 2) {
+            a = Double.parseDouble(args[0]);
+            b = Double.parseDouble(args[1]);
+        }
+        else {
+            a = 16.3;
+            b = 19.2;
+        }
+
+        System.out.println(minimum(a, b));
+    }
+
+    private static double minimum(double a, double b) {
+        if (a < b) {
+            return a;
+        } else {
+            return b;
+        }
+    }
+}
