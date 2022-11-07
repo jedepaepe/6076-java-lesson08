f9730776c504dfb6e72cc1668c05eb7df8d6e11a
Jean-Michel Depaepe <jean-michel.depaepe@equans.com>
2022-11-07 22:29:33 +0100

    exercice

---
+++ src/main/java/com/example/lesson08/DemoMaximum1.java
@@ -0,0 +1,14 @@
+package com.example.lesson08;
+
+public class DemoMaximum1 {
+    public static void main(String[] args) {
+        int a = 9;
+        int b = 16;
+
+        if (a > b) {
+            System.out.println(a);
+        } else {
+            System.out.println(b);
+        }
+    }
+}
