43b50f57e86499b313b8fcbb4beb52d5750f9c29
Jean-Michel Depaepe <jean-michel.depaepe@equans.com>
2022-11-07 22:36:58 +0100

    exercice

--- src/main/java/com/example/lesson08/DemoMaximum1.java
+++ src/main/java/com/example/lesson08/DemoMaximum1.java
@@ -2,8 +2,8 @@ package com.example.lesson08;
 
 public class DemoMaximum1 {
     public static void main(String[] args) {
-        int a = 9;
-        int b = 16;
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
 
         if (a > b) {
             System.out.println(a);
